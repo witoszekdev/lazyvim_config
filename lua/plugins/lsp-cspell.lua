516fdcd5461f23779284811fa57e2d4db70cf9b5
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-10-16 16:15:08 +0200

    Enable cspell only when configured in repo

--- lua/plugins/lsp-cspell.lua
+++ lua/plugins/lsp-cspell.lua
@@ -1,15 +1,25 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "davidmh/cspell.nvim",
-    },
-    opts = function(_, opts)
-      local cspell = require("cspell")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        cspell.diagnostics,
-        cspell.code_actions,
-      })
-    end,
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "davidmh/cspell.nvim",
   },
+  event = { "BufReadPre", "BufNewFile" },
+  opts = function(_, opts)
+    local cspell_diagnostics = require("cspell.diagnostics")
+    local cspell_code_actions = require("cspell.code_actions")
+    local util = require("lazyvim.util")
+
+    local function cspell_config_exists()
+      local git_root = util.root.get()
+      local cspell_config = git_root .. "/.cspell.json"
+      return vim.fn.filereadable(cspell_config) == 1
+    end
+
+    if cspell_config_exists() then
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        cspell_diagnostics,
+        cspell_code_actions,
+      })
+    end
+  end,
 }
